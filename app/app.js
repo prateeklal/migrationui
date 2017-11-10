452a1ca1184e5efc06d9b54c2aba57a04f8b89c2
swathidivya <bsdivya@nisum.com>
2017-11-10 09:47:17 +0530

    bower update

--- app/app.js
+++ app/app.js
@@ -1,9 +1,6 @@
 define(['backbone.marionette','views/loginView'], function(marionette,loginView) {  
   var app =marionette.Application.extend({
-      region: '#main-content',
-    
       onStart() {
-        this.showView(new loginView());
       }
   });
   return app;
